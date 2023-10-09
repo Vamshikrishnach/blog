dbd278a6c34e5c1ad678688cb0ad5cf1b1a52a01
unknown <chandra.v@falconavl.com>
2023-10-09 16:28:43 +0530

    Updated the views and urls

--- myblogapp/views.py
+++ myblogapp/views.py
@@ -4,42 +4,42 @@ from .models import Author
 from .serializer import *
 from rest_framework import status, mixins, generics, viewsets
 
-class AuthorListView(viewsets.GenericViewSet,mixins.ListModelMixin,mixins.CreateModelMixin):
+class AuthorListView(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
 
-class AuthorDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Author.objects.all()
-    serializer_class = AuthorSerializer
+# class AuthorDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Author.objects.all()
+#     serializer_class = AuthorSerializer
 
-class CategoryListView(generics.ListCreateAPIView):
+class CategoryListView(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+# class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializer
 
-class TagListView(generics.ListCreateAPIView):
+class TagListView(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
-class TagDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Tag.objects.all()
-    serializer_class = TagSerializer
+# class TagDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Tag.objects.all()
+#     serializer_class = TagSerializer
 
-class BlogPostListView(generics.ListCreateAPIView):
+class BlogPostListView(viewsets.ModelViewSet):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
 
-class BlogPostDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = BlogPost.objects.all()
-    serializer_class = BlogPostSerializer
+# class BlogPostDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = BlogPost.objects.all()
+#     serializer_class = BlogPostSerializer
 
-class CommentListView(generics.ListCreateAPIView):
+class CommentListView(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
-class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
+# class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
